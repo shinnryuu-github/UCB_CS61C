13fb217d858b054da16d177674bbbbdbe1ab7456
ryuusama <p.k17173@163.com>
2024-02-26 22:33:45 +0800

    lab01 finished

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,21 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (!head)
+        return 0;
+    node *tortoise = head, *hare = head;
+    while (1){
+        if (!hare->next)
+            return 0;
+        else
+            hare = hare->next;
+        if (!hare->next)
+            return 0;
+        else
+            hare = hare->next;
+        tortoise = tortoise->next;
+        if (tortoise == hare)
+            return 1;
+    }
     return 0;
 }
